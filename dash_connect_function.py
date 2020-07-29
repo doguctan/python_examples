62e1e2808349c017dcde8f0b08c891b98b8b5e95
doguctan <68854860+doguctan@users.noreply.github.com>
2020-07-29 22:56:30 +0300

    Update dash_connect_function.py

--- dash_connect_function.py
+++ dash_connect_function.py
@@ -30,3 +30,67 @@ def update_output_div(input_value):
 if __name__=='__main__':
     app.run_server()
 
+#-------------------------
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 20:58:40 2020
+
+@author: doguctan
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+
+df=pd.read_csv('gapminderDataFiveYear.csv')
+
+app=dash.Dash()
+
+year_options=[{'label':str(year),'value':year} for year in df['year'].unique()]
+
+
+
+app.layout=html.Div([
+                dcc.Graph(id='graph'),
+                dcc.Dropdown(id='year_picker', options=year_options,
+                             value=df['year'].min())
+    
+])
+
+@app.callback(Output(component_id='graph',component_property='figure'),
+              [Input(component_id='year_picker',component_property='value')])
+def update_figure(selected_year):
+    
+    
+    #DATA ONLY FOR SELECTED YEAR FROM DROPDOWN
+    filtered_df=df[df['year']==selected_year]
+    traces=[]
+    
+    for continent_name in filtered_df['continent'].unique():
+        df_by_continent=filtered_df[filtered_df['continent']==continent_name]
+        traces.append(go.Scatter(
+                    x=df_by_continent['gdpPercap'],
+                    y=df_by_continent['lifeExp'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={'size':15},
+                    name=continent_name
+            
+            ))
+    
+    
+    return {'data':traces, 
+            'layout':go.Layout(title='My Plot',
+                               xaxis={'title':'GDP Per Cap','type':'log'},
+                               yaxis={'title':'Life Expectancy'})}
+
+if __name__=='__main__':
+    app.run_server()
+
+#-------------------------------------
